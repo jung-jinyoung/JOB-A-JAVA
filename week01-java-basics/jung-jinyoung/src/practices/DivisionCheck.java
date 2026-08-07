2353992c72fb342dfad6dc16f8bcb84953e2a36b
jinyoung jung <ya_1001@naver.com>
2026-08-07 23:26:44 +0900

    feat: 02 원시타입과 오버플로우 정리 및 실습 파일

---
+++ week01-java-basics/jung-jinyoung/src/practices/DivisionCheck.java
@@ -0,0 +1,9 @@
+package practices;
+
+public class DivisionCheck {
+    static void main() {
+        System.out.println(7 / 2); // 정수 나눗셈 : result 3
+        System.out.println((double) 7 /2); // 캐스팅 후 나눗셈 : result 3.5
+        System.out.println((double) (7 / 2)); // 괄호 위치 변경 후 나눗셈
+    }
+}
